f2dcebb5cfbe7c4d839715b007d6d58476cec411
Marcin Kilarski <mr.kilarski@gmail.com>
2021-03-24 22:20:22 +0100

    Refactor defer-async-scripts-loading.php

--- wordpress/page-speed/defer-async-scripts-loading.php
+++ wordpress/page-speed/defer-async-scripts-loading.php
@@ -2,27 +2,34 @@
 namespace My_Site\Page_Speed;
 
 /**
- * Page Speed Improvement: Defer loading all JavaScript files registered via the wp_enqueue_script() function
+ * Page Speed Improvement: Asynchronously load all JavaScript files registered via the wp_enqueue_script() function
  */
-add_filter('script_loader_tag', __NAMESPACE__ . '\\defer_async_scripts', 10);
-function defer_async_scripts($tag)
+add_filter('script_loader_tag', __NAMESPACE__ . '\\defer_async_scripts', 10, 3);
+function defer_async_scripts($tag, $handle, $src)
 {
+    // Finding script handles
+    // log handles of all registered scripts
+    // error_log($handle . ': ' . $handle);
+
+    // add script handle to every the script ID
+    // return '<script id="' . $handle . '" src="' . $src . '"></script>';
+
     // Don't do anything if user is logged in as admin
     if (is_admin()) {
         return $tag;
     }
 
-    // list of scripts to async, e.g. my-script.js
-    $scripts_to_async = [];
+    // list of scripts to defer, e.g. my-script
+    $scripts_to_defer = [];
 
-    // async scripts
-    foreach ($scripts_to_async as $async_script) {
-        if (true == strpos($tag, $async_script)) {
-            return str_replace(' src', ' async="async" src', $tag);
+    // defer scripts
+    foreach ($scripts_to_defer as $defer_script) {
+        if (true == strpos($tag, $defer_script)) {
+            return str_replace(' src', ' defer="defer" src', $tag);
         }
     }
 
-    // Do not add async to these scripts, e.g. my-script.js
+    // Do not add async to these scripts, e.g. jquery
     $scripts_to_exclude = [];
 
     // exclude scripts
@@ -32,5 +39,5 @@ function defer_async_scripts($tag)
         }
     }
 
-    return str_replace(' src', ' defer="defer" src', $tag);
+    return str_replace(' src', ' async="async" src', $tag);
 }
