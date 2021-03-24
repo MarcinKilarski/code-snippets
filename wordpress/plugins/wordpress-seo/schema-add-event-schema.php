ba175cd791d432c3e6abf2d401c93ff9707b93c2
Marcin Kilarski <mr.kilarski@gmail.com>
2021-03-24 17:06:59 +0100

    Update event schema

--- wordpress/plugins/wordpress-seo/schema-add-event-schema.php
+++ wordpress/plugins/wordpress-seo/schema-add-event-schema.php
@@ -15,7 +15,7 @@ namespace My_Site\Plugins\WordPress_SEO;
  * - Yoast: https://developer.yoast.com/features/schema/integration-guidelines/
  * - Yoast: https://developer.yoast.com/features/schema/api/
  * - Google: https://developers.google.com/search/docs/data-types/event
- * - Example: https://github.com/billerickson/Product-Review-Schema-for-Yoast-SEO/blob/7f895948d7d73dbefd9e503a155c9f562ae5f1d0/class-be-product-review.php#L6
+ * - https://github.com/billerickson/Product-Review-Schema-for-Yoast-SEO/blob/7f895948d7d73dbefd9e503a155c9f562ae5f1d0/class-be-product-review.php#L6
  */
 class Event_Scheme {
 	/**
@@ -31,8 +31,8 @@ class Event_Scheme {
 	 * @return bool
 	 */
 	public function is_needed() {
-        // add it only on the page with ID 15848 (event page)
-		if (is_page(15848)) {
+        // CPT name
+		if ( 'webinar' === get_post_type() ) {
             return true;
         }
 		return false;
@@ -44,25 +44,79 @@ class Event_Scheme {
 	 * @return array $graph Review markup
 	 */
 	public function generate() {
+        // get all ACF fields from a post
+        $fields = get_fields();
+
+        // Featured image
+        $featured_image = get_the_post_thumbnail_url();
+        $featured_image_fallback_img_id = 14674;
+        if (!$featured_image) $featured_image = wp_get_attachment_url($featured_image_fallback_img_id);
+
+        // Dates
+        // $todays_date = time();
+        $date_time_format = "Y-m-d\Th:i:s+01:00";
+        $start_date = esc_html($fields['date-time']);
+        $webinar_lenght = esc_html($fields['webinar_length']);
+        $start_date_to_time = strtotime($start_date);
+        $end_date_to_time = strtotime($start_date . ' + ' . $webinar_lenght . ' minutes');
+        $start_date_formated = date($date_time_format, $start_date_to_time);
+        $end_date_formated = date($date_time_format, $end_date_to_time);
+
+        // Speakers info
+        $speakers_output = [];
+        $speakers_ids = $fields['speakers'];
+
+        if ($speakers_ids):
+            foreach ($speakers_ids as $speaker_id):
+                $speaker_linkedin_url = get_field("linkedin_profile_url", $speaker_id);
+
+                $speakers_output[] = [
+                    "@type" => "Person",
+                    "name" => get_the_title($speaker_id),
+                    "sameAs" => $speaker_linkedin_url ?? "",
+                ];
+            endforeach;
+        endif;
+
+        // Terms
+        $terms_output = '';
+        $taxonomies = [
+            'industry',
+            'topics',
+        ];
+
+        foreach ($taxonomies as $index => $tax):
+            $terms = strip_tags( get_the_term_list( $post->ID, $tax, '', ', ' ) );
+
+            // if any terms are found add them to the output
+            if ($terms):
+                if ($terms_output):
+                    $terms_output .= ', ' . $terms;
+                else:
+                    $terms_output .= $terms;
+                endif;
+            endif;
+        endforeach;
+
+        // Output
 		$data = [
             "@type" => "Event",
-            "name" => "Star Wars Family Get Together",
-            "startDate" => "2021-04-19T16:00:00+01:00",
-            "endDate" => "2021-04-21T16:00:00+01:00",
+            "name" => get_the_title(),
+            "startDate" => $start_date_formated,
+            "endDate" => $end_date_formated,
             "eventAttendanceMode" => "https://schema.org/OnlineEventAttendanceMode",
             "eventStatus" => "https://schema.org/EventScheduled",
             "location" => [
                 "@type" => "VirtualLocation",
-                "url" => "https://virtual.hubilo.com/"
+                "url" => "https://zoom.us/webinar/"
             ],
             "image" => [
-                "https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=1%2C318%2C999%2C499",
-                "https://lumiere-a.akamaihd.net/v1/images/solo-a-star-wars-story-theatrical-poster-2_f4af9297.jpeg?region=0%2C397%2C1298%2C646&width=1200",
-                ],
-            "description" => "Join hundreds of Star Wars fans around the world to celebrate the series and meet same-minded people.",
+                $featured_image,
+            ],
+            "description" => wp_kses_post( $fields['webinar_description'] ),
             "audience" => [
                 "@type" => "Audience",
-                "name" => "Sci-fi enthusiasts, Star Wars fans"
+                "name" => "Professionals interested in learning more about " . $terms_output,
             ],
             "inLanguage" => [
                 "@type" => "Language",
@@ -71,35 +125,23 @@ class Event_Scheme {
             "isAccessibleForFree" => true,
             "offers" => [
                 "@type" => "Offer",
-                "url" => "https://www.starwars.com/films",
+                "url" => get_the_permalink(),
                 "price" => "0",
                 "priceCurrency" => "USD",
                 "availability" => "https://schema.org/InStock",
-                "validFrom" => "2021-04-21T16:00"
+                "validFrom" => get_the_date($date_time_format),
             ],
             "organizer" => [
                 "@type" => "Organization",
-                "name" => "Star Wars",
-                "url" => "https://www.starwars.com"
+                "name" => "Company Name",
+                "url" => "https://company-site.com/"
             ],
-            "performer" => [
-                [
-                    "@type" => "Person",
-                    "name" => "Princess Leia",
-                    "sameAs" => "https://www.linkedin.com/in/9823673298378903/",
-                ],
-                [
-                    "@type" => "Person",
-                    "name" => "Luke Skywalker",
-                    "sameAs" => "https://www.linkedin.com/in/2902370382739826398/",
-                ],
-            ],
+            "performer" => $speakers_output,
         ];
 
 		return $data;
 	}
 }
-
 
 /**
  * Adds an event graph piece to the schema collector.
