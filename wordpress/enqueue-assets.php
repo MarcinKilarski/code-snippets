4f55c7141348dd5acc74852e3165667b9e40b128
Marcin Kilarski <mr.kilarski@gmail.com>
2021-03-26 11:15:58 +0100

    Feat enqueue-assets.php

---
+++ wordpress/enqueue-assets.php
@@ -0,0 +1,17 @@
+<?php
+namespace My_Site\Enqueue_Assets;
+
+/**
+ * Enqueue theme CSS and JavaScript in front-end only.
+ */
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_frontend_assets' );
+function enqueue_frontend_assets()
+{
+    // create my own version codes based on when the files were last time modified
+    $my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'my-script.js' ));
+    $my_css_ver = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'my-styles.css' ));
+
+    wp_enqueue_script( 'my-script', plugins_url( 'js/custom.js', __FILE__ ), ['jquery'], $my_js_ver );
+    wp_register_style( 'my-styles', plugins_url( 'style.css', __FILE__ ), false, $my_css_ver );
+    wp_enqueue_style ( 'my_css' );
+}
