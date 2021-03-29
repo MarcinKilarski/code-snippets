c3b8a5a09c82f078cfb0e71648a189af85880e67
Marcin Kilarski <mr.kilarski@gmail.com>
2021-03-29 10:39:43 +0200

    Refactor enqueue-assets.php

--- wordpress/enqueue-assets.php
+++ wordpress/enqueue-assets.php
@@ -7,11 +7,20 @@ namespace My_Site\Enqueue_Assets;
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_frontend_assets' );
 function enqueue_frontend_assets()
 {
-    // create my own version codes based on when the files were last time modified
-    $my_js_ver  = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'my-script.js' ));
-    $my_css_ver = date("ymd-Gis", filemtime( plugin_dir_path( __FILE__ ) . 'my-style.css' ));
+    // theme path and url
+    $theme_dir_path = CHILD_THEME_DIR_PATH;
+    $theme_dir_url = CHILD_THEME_DIR_URL;
 
-    wp_enqueue_script( 'my-script', plugins_url( 'my-script.js', __FILE__ ), ['jquery'], $my_js_ver );
-    wp_register_style( 'my-style', plugins_url( 'my-style.css', __FILE__ ), false, $my_css_ver );
-    wp_enqueue_style ( 'style' );
+    // relative file path
+    $js_file_path = '/assets/js/my-script.js';
+    $css_file_path = '/assets/css/my-styles.css';
+
+    // create version stamp based on when the files were last time modified
+    // this way each version of the asset is safely cached
+    $js_ver  = date("ymd-Gis", filemtime( $theme_dir_path . $js_file_path ));
+    $css_ver = date("ymd-Gis", filemtime( $theme_dir_path . $css_file_path ));
+
+    // enqueue assets
+    wp_enqueue_script( 'my-script', $theme_dir_url . $js_file_path, ['jquery'], $js_ver );
+    wp_enqueue_style( 'my-style', $theme_dir_url . $css_file_path, false, $css_ver );
 }
