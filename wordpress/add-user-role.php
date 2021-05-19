0a63397c5c314f90615226c43a9c234ab4ed2481
Marcin Kilarski <mr.kilarski@gmail.com>
2021-05-19 11:04:42 +0200

    Refactor add-user-role

--- wordpress/add-user-role.php
+++ wordpress/add-user-role.php
@@ -30,42 +30,50 @@ function add_user_role() {
  */
 add_action( 'admin_init', __NAMESPACE__ . '\\modify_user_role');
 function modify_user_role() {
-    $role = get_role( 'event_manager' );
+		$roles = [
+			'administrator',
+			'editor',
+			'event_manager',
+		];
 
-    // access to webinars CPT
-    $role->add_cap( 'read_webinar');
-    $role->add_cap( 'edit_webinar' );
-    $role->add_cap( 'read_private_webinars' );
-    $role->add_cap( 'edit_webinars' );
-    $role->add_cap( 'edit_others_webinars' );
-    $role->add_cap( 'edit_published_webinars' );
-    $role->add_cap( 'publish_webinars' );
-    $role->add_cap( 'delete_others_webinars' );
-    $role->add_cap( 'delete_private_webinars' );
-    $role->add_cap( 'delete_published_webinars' );
+		foreach( $roles as $the_role ) {
+			$role = get_role( $the_role );
 
-    // access to speakers CPT
-    $role->add_cap( 'read_speaker');
-    $role->add_cap( 'edit_speaker' );
-    $role->add_cap( 'read_private_speakers' );
-    $role->add_cap( 'edit_speakers' );
-    $role->add_cap( 'edit_others_speakers' );
-    $role->add_cap( 'edit_published_speakers' );
-    $role->add_cap( 'publish_speakers' );
-    $role->add_cap( 'delete_others_speakers' );
-    $role->add_cap( 'delete_private_speakers' );
-    $role->add_cap( 'delete_published_speakers' );
+			// access to webinars CPT
+			$role->add_cap( 'read_webinar');
+			$role->add_cap( 'edit_webinar' );
+			$role->add_cap( 'read_private_webinars' );
+			$role->add_cap( 'edit_webinars' );
+			$role->add_cap( 'edit_others_webinars' );
+			$role->add_cap( 'edit_published_webinars' );
+			$role->add_cap( 'publish_webinars' );
+			$role->add_cap( 'delete_others_webinars' );
+			$role->add_cap( 'delete_private_webinars' );
+			$role->add_cap( 'delete_published_webinars' );
 
-    // access to topics taxonomy
-    $role->add_cap( 'manage_topic' );
-    $role->add_cap( 'edit_topic' );
-    $role->add_cap( 'delete_topic' );
-    $role->add_cap( 'assign_topic' );
-    $role->add_cap( 'manage_topics' );
-    $role->add_cap( 'edit_topics' );
-    $role->add_cap( 'delete_topics' );
-    $role->add_cap( 'assign_topics' );
+			// access to speakers CPT
+			$role->add_cap( 'read_speaker');
+			$role->add_cap( 'edit_speaker' );
+			$role->add_cap( 'read_private_speakers' );
+			$role->add_cap( 'edit_speakers' );
+			$role->add_cap( 'edit_others_speakers' );
+			$role->add_cap( 'edit_published_speakers' );
+			$role->add_cap( 'publish_speakers' );
+			$role->add_cap( 'delete_others_speakers' );
+			$role->add_cap( 'delete_private_speakers' );
+			$role->add_cap( 'delete_published_speakers' );
 
-    // access to media library
-    $role->add_cap( 'upload_files' );
+			// access to topics taxonomy
+			$role->add_cap( 'manage_topic' );
+			$role->add_cap( 'edit_topic' );
+			$role->add_cap( 'delete_topic' );
+			$role->add_cap( 'assign_topic' );
+			$role->add_cap( 'manage_topics' );
+			$role->add_cap( 'edit_topics' );
+			$role->add_cap( 'delete_topics' );
+			$role->add_cap( 'assign_topics' );
+
+			// access to media library
+			$role->add_cap( 'upload_files' );
+		}
 }
